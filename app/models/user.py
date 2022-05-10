68685a3901bd4d03f22b6a5f6a88261b96e2e959
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-05-10 13:15:56 -0700

    deploying to heroku

--- app/models/user.py
+++ app/models/user.py
@@ -7,8 +7,11 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    profile_picture = db.Column(db.String)
     hashed_password = db.Column(db.String(255), nullable=False)
 
     @property
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        email='demo@aa.io', first_name='Demo', last_name='User', username='Demo', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        email='marnie@aa.io', first_name='Marnie', last_name='Mays', username='marnie', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        email='bobbie@aa.io', first_name='Bobbie', last_name='Brown', username='bobbie',  password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
