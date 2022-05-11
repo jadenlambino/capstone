e0ca39c64a802dc1cdf60ee87bf504d500912f8f
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-05-11 08:47:55 -0700

    finished refactoring signup form and added a listing form

--- app/forms/listing_form.py
+++ app/forms/listing_form.py
@@ -1,9 +1,13 @@
 from unicodedata import name
 from flask import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, SelectField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import Listing
 
 class ListingForm(FlaskForm):
+    product_tag = SelectField('Product Tag', choices=[(1,'Jacket'), (2, 'Shirt'), (3 , 'T-Shirt'), (4 , 'Sweatshirt'), (5 , 'Hoodie'), (6 , 'Jeans'), (7 , 'Pants'), (8 , 'Shorts'), (9 , 'Shoes'), (10 , 'Hats'), (11, 'Accessories')], validators=[DataRequired('Please select a category.')])
     name = StringField('Name', validators=[DataRequired('Please give your listing a name')])
+    price = FloatField('Price', validators=[DataRequired('Please set a price for your listing.')])
+    description = StringField('Description', validators=[DataRequired('Tell users why they should buy your product!')])
+    photos = StringField('Photos')
