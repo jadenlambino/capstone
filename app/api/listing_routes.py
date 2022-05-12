901fcd273629a5b38d094c92335acda3b33da0b9
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-05-12 11:11:38 -0700

    lunch

--- app/api/listing_routes.py
+++ app/api/listing_routes.py
@@ -20,14 +20,32 @@ def post_listing():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         data = form.data
-        print("++++++++++++++" + str(data))
+        # print("++++++++++++++" + str(data))
+
+        if "image" not in request.files:
+            return {"errors": "image required"}, 400
+
+        image = request.files["image"]
+
+        if not allowed_file(image.filename):
+            return {"errors": "file type not permitted"}, 400
+
+        image.filename = get_unique_filename(image.filename)
+
+        upload = upload_file_to_s3(image)
+
+        if "url" not in upload:
+            return upload, 400
+
+        url = upload['url']
+
         new_listing = Listing (
             user_id = current_user.id,
             product_tag = data['product_tag'],
             name = data['name'],
             price = data['price'],
             description = data['description'],
-            photos = data['photos']
+            photos = url
         )
         print('++++++++++++++' +  str(data))
         db.session.add(new_listing)
--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -9,7 +9,7 @@ ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
 s3 = boto3.client(
     "s3",
-    aws_access_key=os.environ.get("S3_KEY"),
+    aws_access_key_id=os.environ.get("S3_KEY"),
     aws_secret_access_key=os.environ.get("S3_SECRET")
 )
 
