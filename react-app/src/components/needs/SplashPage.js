af51e69a7849b1d41c5b87679dc96c0109f005dd
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-08-02 00:50:02 -0700

    added pictures and a carousel, need to resize pictures so they arent ugly as hell

--- react-app/src/components/needs/SplashPage.js
+++ react-app/src/components/needs/SplashPage.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import Carousel from 'react-multi-carousel'
+import "react-multi-carousel/lib/styles.css";
 import './SplashPage.css'
 
 const SplashPage = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     return (
         <div className="splash-div">
             <h1 className='splash'>WELCOME TO JAILED</h1>
             <h2 className='splash'>DEALS SO GOOD THEY'RE CRIMINAL</h2>
+            <div className='carousel-container'>
+                <h3>Shop by Category</h3>
+                <Carousel
+                    responsive={responsive}
+                    className='carousel'
+                >
+                    <div className='c-jacket'></div>
+                    <div className='c-shirt'></div>
+                    <div className='c-tshirt'></div>
+                    <div className='c-sweatshirt'></div>
+                    <div className='c-hoodie'></div>
+                    <div className='c-jeans'></div>
+                    <div className='c-pants'></div>
+                    <div className='c-shorts'></div>
+                    <div className='c-shoes'></div>
+                    <div className='c-hats'></div>
+                    <div className='c-accessories'></div>
+                </Carousel>
+            </div>
+            <div className='description'>
+
+            </div>
         </div>
     )
 }
