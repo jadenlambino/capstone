2d63f918ed67e186730b33199b5fad2019e6f69b
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2023-01-17 02:28:40 -0800

    modified db.py

--- app/models/db.py
+++ app/models/db.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# import and set acriable to access flask environment
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get('SCHEMA')
+
 db = SQLAlchemy()
+
+# add function to add a prefic to table names in production environment only
+def add_prefix_for_prod(attr):
+    if environment == "production":
+        return f"{SCHEMA}.{attr}"
+    else:
+        return attr
