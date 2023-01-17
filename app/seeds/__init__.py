6ef29822d4c8f12c12601277f1ef2cb0b2cbee09
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2023-01-17 03:09:13 -0800

    adjusted seed commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,7 @@ from .users import seed_users, undo_users
 from .product_tags import seed_product_tags, undo_product_tags
 from .listings import seed_listings, undo_listings
 from .reviews import seed_reviews, undo_reviews
+from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,9 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.commit()
     seed_users()
     seed_product_tags()
     seed_listings()
